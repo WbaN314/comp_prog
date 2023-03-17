cf046370c598865702cec1b562df8c0d012bc888
SourLime <limejuicestudio@gmail.com>
2023-03-17 15:39:53 +0100

    renaming

--- src/n0010h_regular_expressin_matching.rs
+++ src/n0010h_regular_expressin_matching.rs
@@ -10,37 +10,34 @@ impl Solution {
         let s = s.as_bytes();
         let p = p.as_bytes();
 
-        let tokens = Self::token_vec(&p);
+        let tokens = Self::parse_regex(&p);
+        println!("{tokens:?}");
 
-        return Self::solve(&s, &p, &tokens);
+        return Self::solve(&s, &tokens);
     }
 
-    fn solve(s: &[u8], p: &[u8], tokens: &Vec<(usize, usize)) -> bool {
-        while let 
-    }
-
-    fn get_potential_match(s: &[u8], token: (usize, usize), start: usize) -> usize {
-
+    fn solve(s: &[u8], tokens: &Vec<&[u8]>) -> bool {
+        return true 
     }
 
     // make token vector splitting regex at .* and <char>* expressions
-    fn token_vec(p: &[u8]) -> Vec<(usize, usize)> {
-        let mut v: Vec<(usize, usize)> = Vec::new();
+    fn parse_regex(p: &[u8]) -> Vec<&[u8]> {
+        let mut v: Vec<&[u8]> = Vec::new();
         let mut i_l= 0;
         let mut i_r = 0;
         loop {
             match p.get(i_r) {
                 None => {
-                    if i_l <= i_r - 1 {
-                        v.push((i_l, i_r -1))
+                    if i_l + 1 <= i_r {
+                        v.push(&p[i_l..=i_r -1])
                     };
                     break;
                 },
                 Some(r) if r == &b'*' => {
-                    if i_l <= i_r - 2 {
-                        v.push((i_l, i_r - 2));
+                    if i_l + 2 <= i_r {
+                        v.push(&p[i_l..=i_r - 2]);
                     }
-                    // v.push((i_r - 1, i_r)); keeping the * tokens out of the vec is enough for this problem
+                    v.push(&p[i_r - 1..=i_r]);
                     i_r += 1;
                     i_l = i_r;
                 },
@@ -61,7 +58,8 @@ impl Solution {
  
      #[test]
      fn test_1() {
-        println!("{:?}", Solution::token_vec("aaaaa*aaaaa.*ddddd".as_bytes()));
-        // assert_eq!(false ,Solution::is_match("a".to_string(), "aa*aaaa.*xc".to_string()));
+        let s = String::from("aaa");
+        let p = String::from(".*.*aaabbb.*aaa");
+        assert_eq!(false, Solution::is_match(s, p));
     }
  }
