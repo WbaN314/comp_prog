38a7f21b94e14c3c141ec6ff16f53b2a94cf92c1
SourLime <limejuicestudio@gmail.com>
2023-03-22 13:20:23 +0100

    0015 improved runtime

--- src/n0015m_3sum.rs
+++ src/n0015m_3sum.rs
@@ -3,28 +3,37 @@ pub struct Solution {}
 
 // START SUBMISSION CODE
 
-use std::collections::HashSet;
-use std::iter::FromIterator;
 impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut solution: Vec<Vec<i32>> = vec![];
-        nums.sort_unstable();
-        let set: HashSet<i32> = HashSet::from_iter(nums.clone());
 
-        for i in 0..nums.len()-2 {
-            for j in i+1..nums.len() - 1 {
-                let req = -(nums[i] + nums[j]);
-                if set.contains(&req) {
-                    if req < nums[j] {
-                        break
-                    } else if req == nums[j] && req != nums[j+1] {
-                        break
+        let mut solution: Vec<Vec<i32>> = vec![];
+        if nums.len() == 0 {
+            return solution
+        }
+
+        nums.sort_unstable();
+
+        for l in 0..nums.len()-2 {
+            if l > 0 && nums[l] == nums[l - 1] {continue};
+            let mut m = l+1;
+            let mut r = nums.len() - 1;
+            while m < r {
+                let val = nums[l] + nums[m] + nums[r];
+                match (val > 0, val < 0) {
+                    (true, _) => r -= 1,
+                    (_, true) => m += 1,
+                    _ => {
+                        solution.push(vec![nums[l], nums[m], nums[r]]);
+                        m += 1;
+                        while nums[m] == nums[m - 1] && m < r {
+                            m += 1;
+                        }
                     }
-                    solution.push(vec![nums[i], nums[j], req])
                 }
             }
-        }
-        HashSet::<Vec<i32>>::from_iter(solution).into_iter().collect()
+        };
+
+        solution
     }
 }
  
