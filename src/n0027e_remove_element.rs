fa9a57f5bba152043be98b1174ed35f6771b01e8
SourLime <limejuicestudio@gmail.com>
2023-04-17 16:35:29 +0200

    0027 done

---
+++ src/n0027e_remove_element.rs
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems
+pub struct Solution {}
+
+// START SUBMISSION CODE
+
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        nums.retain(|&x| x != val);
+        nums.len() as i32
+    }
+}
+ 
+// END SUBMISSION CODE
+ 
+ #[cfg(test)]
+ mod tests {
+     use super::*;
+ 
+     #[test]
+     fn test_1() {
+     }
+ }
