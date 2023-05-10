4b2e89deac127793f537c8f7282d566f92620f35
SourLime <limejuicestudio@gmail.com>
2023-05-10 16:43:23 +0200

    0042 done, improved to 0ms

--- src/n0042h_trapping_rain_water.rs
+++ src/n0042h_trapping_rain_water.rs
@@ -6,21 +6,37 @@ pub struct Solution {}
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
         
-        let mut stack: Vec<usize> = Vec::new();
+        let mut stack: Vec<(usize, i32)> = Vec::with_capacity(height.len());
         let mut solution = 0;
         let mut n: i32;
+        let mut h_diff;
 
         for i in 1..height.len() {
             n = height[i-1] - height[i];
 
             if n > 0 {
-                for _ in 0..n {
-                    stack.push(i);
-                }
+
+                stack.push((i, n));
+
             } else if n < 0 {
-                for _ in 0..-n {
-                    if let Some(v) = stack.pop() {
-                        solution += i - v;
+                n = -n;
+                loop {
+                    match stack.pop() {
+                        None => break,
+                        Some((i_l, n_l)) => {
+                            h_diff = n_l - n;
+                            if h_diff > 0 {
+                                solution += n * (i as i32 - i_l as i32);
+                                stack.push((i_l, h_diff));
+                                break
+                            } else if h_diff == 0 {
+                                solution += n_l * (i as i32 - i_l as i32);
+                                break
+                            } else {
+                                solution += n_l * (i as i32 - i_l as i32);
+                                n -= n_l;
+                            }
+                        }
                     }
                 }
             }
@@ -38,5 +54,9 @@ impl Solution {
      #[test]
      fn test_1() {
         assert_eq!(Solution::trap(vec![0,1,0,2,1,0,1,3,2,1,2,1]), 6);
+     }
+     #[test]
+     fn test_2() {
+        assert_eq!(Solution::trap(vec![6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3]), 83);
      }
  }
